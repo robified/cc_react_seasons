a3716b3044e3a96320aee7ae0d6069affe24c6e9
Robin <daorob@gmail.com>
2020-03-02 07:43:47 -0600

    Create SeasonDisplay component and successfully retrieve geolocation of user.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
+    window.navigator.geolocation.getCurrentPosition(
+        // the success callback
+        position => console.log(position),
+        // the failure callback
+        error => console.log(error)
+    );
+
     return <div>Herro there!</div>;
 };
 
